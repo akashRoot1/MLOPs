212f7e0ecc08546fd355b8bfede7d896744457d9
akashRoot1 <L00187419@atu.ie>
2025-05-30 12:33:51 +0100

    Update test_train.py

--- model/test_train.py
+++ model/test_train.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pickle
 
 def test_prediction_above_zero():
-    model = pickle.load(open('model.pkl', 'rb'))
+    model = pickle.load(open('model/model.pkl', 'rb'))
     test_data = pd.DataFrame({'TV': [100, 200]})
     predictions = model.predict(test_data)
     assert all(p > 0 for p in predictions), "Predictions not greater than 0"
